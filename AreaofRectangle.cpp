86f460290ff422fcaafaf821ee9777859a1e435a
Habib Ur Rahman <hu045444@gmail.com>
2025-10-30 02:01:38 -0700

    Add files via upload

---
+++ AreaofRectangle.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	//area of rectangle Area=Width*lenght
+	cout<<"\n\n\t ..........~| This is Area of Rectangle Calculater |~............"<<endl;
+	float length, width;
+	cout<<"\n\n\tEnter Value of length :";
+	cin>>length;
+	cout<<"\n\n\tEnter Value of Width  :";
+	cin>>width;
+	
+	cout<<"\n\n\t*********************************"<<endl;
+	
+	float A=length*width;	
+	cout<<"\n\n\tThe Total Area of Rectangle is :"<<A<<endl<<endl;
+	cout<<"\n\n\t***********************"<<endl;
+	
+	
+}
